9f039198547206f81c051a28544557a91c40d21f
riyadhswe <riyadhswe@gmail.com>
2022-04-12 21:02:37 +0600

    New Api Intregration

--- App/urls.py
+++ App/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'App'
+
 urlpatterns = [
-    path('', views.index,name = 'index')
+    path('', views.index,name = 'index'),
+    path('action/', views.action,name = 'action'),
+    path('comedy/', views.comedy,name = 'comedy'),
 ]
--- App/views.py
+++ App/views.py
@@ -12,3 +12,21 @@ def index(request):
     except:
         api = "Error"
     return render(request, 'index.html', {'api': api})
+
+
+def action(request):
+    api_request = requests.get("https://drfmovie.herokuapp.com/action/?format=json")
+    try:
+        api = json.loads(api_request.content)
+    except:
+        api = "Error"
+    return render(request, 'action.html', {'api': api})
+
+
+def comedy(request):
+    api_request = requests.get("https://drfmovie.herokuapp.com/comedy/?format=json")
+    try:
+        api = json.loads(api_request.content)
+    except:
+        api = "Error"
+    return render(request, 'comedy.html', {'api': api})
